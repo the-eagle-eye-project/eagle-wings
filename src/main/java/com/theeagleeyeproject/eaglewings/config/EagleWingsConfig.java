308dd84eac9904735787b42bb0f1939dca102c58
John Martinez <jmartinez5120@gmail.com>
2023-11-14 22:53:29 -0500

    Testing automatic version update to the project using GitHub Actions.

---
+++ src/main/java/com/theeagleeyeproject/eaglewings/config/EagleWingsConfig.java
@@ -0,0 +1,9 @@
+package com.theeagleeyeproject.eaglewings.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.theeagleeyeproject.eaglewings")
+public class EagleWingsConfig {
+}
