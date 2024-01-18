c157bff813d7725e92e3df12249c7c4451a38066
johnmartinez <jmartinez5120@gmail.com>
2024-01-17 23:25:40 -0500

    Adding a generic to handle the service request.

--- src/main/java/com/theeagleeyeproject/eaglewings/validator/BaseServiceValidator.java
+++ src/main/java/com/theeagleeyeproject/eaglewings/validator/BaseServiceValidator.java
@@ -3,9 +3,10 @@ package com.theeagleeyeproject.eaglewings.validator;
 /**
  * {@link BaseServiceValidator} validator interface that ensures the contract for all the service validators are the same.
  *
+ * @param <I> generic type that references the service request
  * @author John Robert Martinez
  */
-public interface BaseServiceValidator {
+public interface BaseServiceValidator<I> {
 
-    <I> void validate(I request);
+    void validate(I request);
 }
